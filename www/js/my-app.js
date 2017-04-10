50047b83e5ee79e75147a786ce45a6d2c79bdf7e
sivaramasaba <sivaramasaba@gmail.com>
2017-04-10 17:58:12 +0530

    Add files via upload

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -90,10 +90,11 @@ $$(document).on('click', '#test_1', function (e) {
         }else{
             console.warn("Google Maps not available - falling back to user selection");
             app = launchnavigator.APP.USER_SELECT;
+            window.open('https://www.google.com/maps/dir/13.0408454,80.2346172/13.0408454,80.23');
         }
-        launchnavigator.navigate([13.0408454,80.23], {
+       /* launchnavigator.navigate([13.0408454,80.23], {
             app: app,
             start: "13.0408454,80.2346172"
-        });
+        });*/
     });
 })
