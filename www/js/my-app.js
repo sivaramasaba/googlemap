f21c0ed74a447d56c67eb9dec0f2b57dd41b4563
sivaramasaba <sivaramasaba@gmail.com>
2017-04-11 13:49:12 +0530

    Add files via upload

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -70,7 +70,7 @@ $$(document).on('click', '#test', function (e) {
          scheme = 'com.google.android.apps.maps';
     } else if(platform == "ios") {
          scheme = 'comgooglemaps';
-         scheme = 'comgooglemaps://';
+         //scheme = 'comgooglemaps://';
     }
     
     myApp.alert("Check App availability scheme" + scheme);
