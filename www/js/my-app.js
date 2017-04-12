2a348444bb0cff21fe3842ef6706cefd76c5161b
sivaramasaba <sivaramasaba@gmail.com>
2017-04-12 09:53:16 +0530

    Add files via upload

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -14,24 +14,6 @@ var mainView = myApp.addView('.view-main', {
 // Handle Cordova Device Ready Event
 $$(document).on('deviceready', function() {
     console.log("Device is ready!");
-    var test="https://maps.googleapis.com/maps/api/directions/json?origin=13.0851998,80.2421413&destination=13.0408454,80.23";
-    console.log(test);
-   // var data = file_get_contents('https://maps.googleapis.com/maps/api/directions/json?origin=13.0851998,80.2421413&destination=13.0408454,80.23');
-    //console.log(data);
-    
-    $.get('https://maps.googleapis.com/maps/api/directions/json?origin=13.0408454,80.2346172&destination=13.0408454,80.23', function(data) {
-         console.log(data);
-         console.log(data.routes[0].legs[0].steps);
-        var steps=data.routes[0].legs[0].steps;
-        var stepByStep ="|13.0408454,80.2346172";
-        for(var i=0;i<steps.length;i++) {
-            console.log()
-            stepByStep =stepByStep +"|"+ steps[i].end_location.lat+','+steps[i].end_location.lng
-        }
-        //stepByStep=stepByStep +"|"+"13.0408454,80.23";
-       // $("#myimage").attr('src','https://maps.googleapis.com/maps/api/staticmap?&size=300x300&maptype=roadmap&markers=color:blue%7Clabel:A%7C13.0408454,80.2346172&markers=color:green%7Clabel:B%7C13.0408454,80.23&markers=color:red%7Clabel:C%7C13.0408454,80.237&path=color:0x0000ff|weight:5'+stepByStep);
-        
-    });
 });
 
 
@@ -61,234 +43,31 @@ $$(document).on('pageInit', '.page[data-page="about"]', function (e) {
 })
 
 $$(document).on('click', '#test', function (e) {
-   // myApp.alert('Here comes test click'+$(this).attr('lat'));
-   
     var platform = device.platform.toLowerCase();
     var scheme ="";
-    //myApp.alert("Platform : "+ platform);
     if(platform == "android") {
          scheme = 'com.google.android.apps.maps';
     } else if(platform == "ios") {
-         scheme = 'comgooglemaps';
          scheme = 'maps://';
-         //scheme = 'comgooglemaps://';
     } else {
         scheme = 'comgooglemaps';
     }
     
-    myApp.alert("Check App availability scheme" + scheme);
     appAvailability.check(
         scheme,       // URI Scheme or Package Name 
         function() {  // Success callback 
-            //console.log(scheme + ' is available :)');
-            myApp.alert("App Available");
-            var addressLongLat = $(this).attr('lat')+','+$(this).attr('lng');
-            var addressLongLat = '13.0408454,80.2346172';
+            var addressLongLat = '53.22921909999999,-4.129498000000012';
             if(platform == "android") {
-                //scheme = 'com.google.android.apps.maps';
                 window.open("geo:"+addressLongLat+"?q="+addressLongLat);
             } else if(platform == "ios") {
-                // scheme = 'comgooglemaps';
                 window.open("http://maps.apple.com/?q="+addressLongLat, '_system');
             }
         },
         function() {  // Error callback 
-            //console.log(scheme + ' is not available :(');
-           // myApp.alert("App not Available");
-             var addressLongLat = $(this).attr('lat')+','+$(this).attr('lng');
-            var addressLongLat = '13.0408454,80.2346172';
-             window.open("http://maps.google.com/?q="+addressLongLat, '_system');
-        }
-    );
-})
-
-
-$$(document).on('click', '#test_1', function (e) {
-   // myApp.alert('Here comes test_1 click');
-   
-    var platform = device.platform.toLowerCase();
-    var scheme ="";
-    myApp.alert("Platform : "+ platform);
-    if(platform == "android") {
-         scheme = 'com.google.android.apps.maps';
-    } else if(platform == "ios") {
-         scheme = 'comgooglemaps://';
-        //scheme = 'http://maps.apple.com/';
-        
-    } else {
-         scheme = 'comgooglemaps://';
-    }
-    
-     myApp.alert("Check App availability"+ scheme);
-    appAvailability.check(
-        scheme,       // URI Scheme or Package Name 
-        function() {  // Success callback 
-            //console.log(scheme + ' is available :)');
-            myApp.alert("App Available");
             var addressLongLat = $(this).attr('lat')+','+$(this).attr('lng');
-            var addressLongLat = '13.0408454,80.2346172';
-            if(platform == "android") {
-                //scheme = 'com.google.android.apps.maps';
-               // window.open("geo:"+addressLongLat+"?q=13.0408454,80.2346172");
-                window.open("http://maps.google.com/?q="+addressLongLat, '_system');
-            } else if(platform == "ios") {
-                // scheme = 'comgooglemaps';
-                window.open("http://maps.apple.com/?q="+addressLongLat, '_system');
-               // window.open("http://maps.google.com/?q="+addressLongLat, '_system');
-            }
-        },
-        function() {  // Error callback 
-            //console.log(scheme + ' is not available :(');
-            myApp.alert("App not Available");
-             var addressLongLat = $(this).attr('lat')+','+$(this).attr('lng');
-            var addressLongLat = '13.0408454,80.2346172';
-             window.open("http://maps.google.com/?q="+addressLongLat, '_system');
+            var addressLongLat = '53.22921909999999,-4.129498000000012';
+            window.open("http://maps.google.com/?q="+addressLongLat, '_system');
         }
     );
 })
 
-
-
-$$(document).on('click', '#test_2', function (e) {
-    myApp.alert('Here comes whatsapp click');
-   
-    var platform = device.platform.toLowerCase();
-    var scheme ="";
-    myApp.alert("Platform : "+ platform);
-    if(platform == "android") {
-         scheme = 'com.google.android.apps.maps';
-    } else if(platform == "ios") {
-         scheme = 'whatsapp://';
-        // scheme = 'comgooglemaps';
-    } else {
-         scheme = 'comgooglemaps://';
-    }
-    
-   myApp.alert("Check App availability"+ scheme);
-    appAvailability.check(
-        scheme,       // URI Scheme or Package Name 
-        function() {  // Success callback 
-            //console.log(scheme + ' is available :)');
-            myApp.alert("App Available");
-            var addressLongLat = $(this).attr('lat')+','+$(this).attr('lng');
-            var addressLongLat = '13.0408454,80.2346172';
-            if(platform == "android") {
-                //scheme = 'com.google.android.apps.maps';
-               // window.open("geo:"+addressLongLat+"?q=13.0408454,80.2346172");
-                window.open("http://maps.google.com/?q="+addressLongLat, '_system');
-            } else if(platform == "ios") {
-                // scheme = 'comgooglemaps';
-               window.open("http://maps.apple.com/?q="+addressLongLat, '_system');
-                //window.open("http://maps.google.com/?q="+addressLongLat, '_system');
-            }
-        },
-        function() {  // Error callback 
-            //console.log(scheme + ' is not available :(');
-            myApp.alert("App not Available");
-             var addressLongLat = $(this).attr('lat')+','+$(this).attr('lng');
-            var addressLongLat = '13.0408454,80.2346172';
-             window.open("http://maps.google.com/?q="+addressLongLat, '_system');
-        }
-    );
-})
-
-
-/*
-$$(document).on('click', '#test', function (e) {
-    myApp.alert('Here comes test click');
-    launchnavigator.isAppAvailable(launchnavigator.APP.GOOGLE_MAPS, function(isAvailable){
-        var app;
-        //myApp.alert('isAvailable');
-       // myApp.alert(isAvailable);
-        if(isAvailable){
-            app = launchnavigator.APP.GOOGLE_MAPS;
-        }else{
-            console.warn("Google Maps not available - falling back to user selection");
-            app = launchnavigator.APP.USER_SELECT;
-        }
-        launchnavigator.navigate([13.0408454,80.23], {
-            app: app,
-            start: "13.0408454,80.2346172"
-        });
-    });
-})
-*/
-
-$$(document).on('click', '#test_5', function (e) {
-  //  myApp.alert('Here comes test click');
-   // launchnavigator.isAppAvailable(launchnavigator.APP.GOOGLE_MAPS, function(isAvailable){
-      //  var app;
-        //window.open('https://www.google.com/maps/dir/13.0408454,80.2346172/13.0408454,80.23');
-      //  myApp.alert('isAvailable');
-      //  myApp.alert(isAvailable);
-       /* if(!isAvailable){
-            app = launchnavigator.APP.GOOGLE_MAPS;
-        }else{
-            myApp.alert('else');
-            console.warn("Google Maps not available - falling back to user selection");
-          //  app = launchnavigator.APP.USER_SELECT;
-            window.open('https://www.google.com/maps/dir/13.0408454,80.2346172/13.0408454,80.23');
-        }*/
-       /* launchnavigator.navigate([13.0408454,80.23], {
-            app: app,
-            start: "13.0408454,80.2346172"
-        });*/
-  //  });
-    
-     // myApp.alert('Here comes whatsapp click');
-   
-    var platform = device.platform.toLowerCase();
-    var scheme ="";
-    myApp.alert("Platform : "+ platform);
-    if(platform == "android") {
-         scheme = 'com.google.android.apps.maps';
-    } else if(platform == "ios") {
-         scheme = 'whatsapp://';
-        
-        // scheme = 'comgooglemaps';
-    } else {
-         scheme = 'comgooglemaps://';
-    }
-    
-   myApp.alert("Check App availability"+ scheme);
-    appAvailability.check(
-        scheme,       // URI Scheme or Package Name 
-        function() {  // Success callback 
-            //console.log(scheme + ' is available :)');
-            myApp.alert("App Available");
-        },
-        function() {  // Error callback 
-            //console.log(scheme + ' is not available :(');
-            myApp.alert("App not Available");
-        }
-    );
-    
-    
-})
-
-
-$$(document).on('click', '#test_6', function (e) {
-  //  myApp.alert('Here comes test click');
-   launchnavigator.isAppAvailable(launchnavigator.APP.GOOGLE_MAPS, function(isAvailable){
-        var app;
-        //window.open('https://www.google.com/maps/dir/13.0408454,80.2346172/13.0408454,80.23');
-        myApp.alert('isAvailable');
-        myApp.alert(isAvailable);
-        if(isAvailable){
-            app = launchnavigator.APP.GOOGLE_MAPS;
-        }else{
-            myApp.alert('else');
-          //  console.warn("Google Maps not available - falling back to user selection");
-            app = launchnavigator.APP.USER_SELECT;
-            //window.open('https://www.google.com/maps/dir/13.0408454,80.2346172/13.0408454,80.23');
-        }
-        launchnavigator.navigate([13.0408454,80.23], {
-            app: app,
-            start: "13.0408454,80.2346172"
-        });
-   });
-    
-
-    
-    
-})
